4af4499777faca62dc9407fdc072130bc5554f63
Koray Cetin <korayctnx06@gmail.com>
2022-05-22 14:51:25 +0300

    some filtering methods were added

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/movie',movieRouter);
+app.use('/api/movies',movieRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,7 +39,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({error:{message : err.message,status: err.code}});
 });
 
 app.listen(3000);
--- routes/movie.js
+++ routes/movie.js
@@ -1,11 +1,68 @@
 const express = require('express');
 const router = express.Router();
-const MovieSchema = require('../models/Movie');
+const Movie = require('../models/Movie');
 
 /* GET home page. */
+
+router.get('/',(req,res)=>{
+  const promise = Movie.find({ });
+
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
+})
+router.get('/:movieId',(req,res,next)=>{
+
+    const promise = Movie.findById(req.params.movieId);
+
+    promise.then((data)=>{
+      if(!data){
+        next({message:"The movie was not found",code:404});
+      }
+      else{
+        res.json(data)
+      }
+    }).catch((err)=>{
+      res.json(err);
+    })
+})
+
+router.put('/:movieId',(req,res,next)=>{
+  const promise = Movie.findByIdAndUpdate(req.params.movieId,req.body,{new:true});
+
+  promise.then((data)=>{
+    if(!data){
+      next({message:"The movie was not found",code:404})
+    }
+    else{
+      res.json({movieName:data.title,});
+    }
+  }).catch((err)=>{
+    res.json(err);
+  })
+})
 router.post('/', (req,res)=>{
-  const data = req.body.title;
-  res.json(data);
+  const {title,imdb_score,category,country,year} = req.body;
+
+  const movie = new Movie({
+    title:title,
+    imdb_score:imdb_score,
+    category:category,
+    country:country,
+    year:year
+  })
+  movie.save((err,data)=>{
+    if(err){
+      res.json(err)
+    }
+    else{
+      res.json(data);
+    }
+  })
 });
+
+
 
 module.exports = router;
